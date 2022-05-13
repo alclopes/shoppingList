3deede534f0ad2026a2d3fe3dadebb05621bbb14
alclopes <alclopes@gmail.com>
2022-05-13 17:39:56 -0300

    feat: add component AppContainer

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,10 +1,16 @@
 import { Wrapper, Container, Title } from './App.styles.js'
+import AppContainer from '../AppContainer'
 
 function App() {
   return (
     <Wrapper>
       <Container>
         <Title>Hello World !</Title>
+        <AppContainer
+          left={<div>produtos disponíveis</div>}
+          middle={<div>lista de compras</div>}
+          right={<div>estatísticas</div>}
+        />
       </Container>
     </Wrapper>
   )
