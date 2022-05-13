40d5cfa711bb4dd0162f999509360f31ded0a4c8
alclopes <alclopes@gmail.com>
2022-05-13 19:59:37 -0300

    feat: add shared LineChart

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,6 +1,6 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import Checkbox from '../../shared/Checkbox'
-
+import LineChart from '../../shared/LineChart'
 import AppContainer from '../AppContainer/AppContainer'
 import AppHeader from '../AppHeader'
 import { Wrapper, Container } from './App.styles'
@@ -8,6 +8,16 @@ import { Wrapper, Container } from './App.styles'
 function App() {
   const [lettuce, setLettuce] = useState(true)
   const [rice, setRice] = useState(false)
+  const [healthy, setHealthy] = useState(20)
+
+  const colors = ['#62CBC6', '#00ABAD', '#00858C', '#006073', '#004D61']
+
+  useEffect(() => {
+    setTimeout(() => {
+      setHealthy(50)
+    }, 5000)
+  }, [])
+
   return (
     <Wrapper>
       <Container>
@@ -28,8 +38,41 @@ function App() {
               />
             </div>
           }
-          middle={<div>lista de compras</div>}
-          right={<div>estatísticas</div>}
+          middle={
+            <div>
+              sua lista de compras
+              <Checkbox
+                value={rice}
+                title='Arroz'
+                onClick={() => setRice(!rice)}
+              />
+            </div>
+          }
+          right={
+            <div>
+              estatísticas
+              <LineChart
+                color={colors[0]}
+                title='saudável'
+                percentage={healthy - 30}
+              />
+              <LineChart
+                color={colors[1]}
+                title='nao tão saudável'
+                percentage={healthy - 10}
+              />
+              <LineChart
+                color={colors[2]}
+                title='limpeza'
+                percentage={healthy + 5}
+              />
+              <LineChart
+                color={colors[3]}
+                title='outros'
+                percentage={healthy - 15}
+              />
+            </div>
+          }
         />
       </Container>
     </Wrapper>
