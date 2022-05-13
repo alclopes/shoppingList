89f18b3ad633e243683bc4cca27c9284c40842cf
alclopes <alclopes@gmail.com>
2022-05-13 19:25:44 -0300

    feat: add shared Checkbox

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,14 +1,33 @@
-import { Wrapper, Container } from './App.styles.js'
-import AppContainer from '../AppContainer'
+import React, { useState } from 'react'
+import Checkbox from '../../shared/Checkbox'
+
+import AppContainer from '../AppContainer/AppContainer'
 import AppHeader from '../AppHeader'
+import { Wrapper, Container } from './App.styles'
 
 function App() {
+  const [lettuce, setLettuce] = useState(true)
+  const [rice, setRice] = useState(false)
   return (
     <Wrapper>
       <Container>
         <AppHeader />
         <AppContainer
-          left={<div>produtos disponíveis</div>}
+          left={
+            <div>
+              produtos disponíveis:
+              <Checkbox
+                value={lettuce}
+                title='Lettuce'
+                onClick={() => setLettuce(!lettuce)}
+              />
+              <Checkbox
+                value={rice}
+                title='Rice'
+                onClick={() => setRice(!rice)}
+              />
+            </div>
+          }
           middle={<div>lista de compras</div>}
           right={<div>estatísticas</div>}
         />
