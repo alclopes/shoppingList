cf1a3b2e93ff670e381426675066db61841dc65c
alclopes <alclopes@gmail.com>
2022-05-14 14:46:55 -0300

    feat: add mock products list

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,28 +1,27 @@
-import React, { useEffect, useState } from 'react'
+import React, { useState } from 'react'
 import LineChart from '../../shared/LineChart'
 import AppContainer from '../AppContainer/AppContainer'
 import AppHeader from '../AppHeader'
 import ShoppingList from '../ShoppingList'
 import { Wrapper, Container } from './App.styles'
+import productsMock from '../../mocks/products.json'
 
 function App() {
-  const [healthy, setHealthy] = useState(20)
+  const [healthy] = useState(20)
 
   const colors = ['#62CBC6', '#00ABAD', '#00858C', '#006073', '#004D61']
 
-  useEffect(() => {
-    setTimeout(() => {
-      setHealthy(50)
-    }, 5000)
-  }, [])
+  const [products, setProducts] = useState(productsMock.products)
 
   return (
     <Wrapper>
       <Container>
         <AppHeader />
         <AppContainer
-          left={<ShoppingList title='Lista de Produtos' />}
-          middle={<ShoppingList title='Sua Lista de Compras' />}
+          left={<ShoppingList title='Lista de Produtos' products={products} />}
+          middle={
+            <ShoppingList title='Sua Lista de Compras' products={products} />
+          }
           right={
             <div>
               estatísticas
--- src/components/ShoppingList/ShoppingList.jsx
+++ src/components/ShoppingList/ShoppingList.jsx
@@ -2,15 +2,18 @@ import React from 'react'
 import { Wrapper, Title, Array } from './ShoppingList.styles'
 import Checkbox from '../../shared/Checkbox'
 
-function ShoppingList({ title }) {
+function ShoppingList({ title, products }) {
   return (
     <Wrapper>
       <Title>{title}:</Title>
       <Array>
-        <Checkbox value={true} title='Alface' />
-        <Checkbox value={false} title='Arroz' />
-        <Checkbox value={true} title='Batata' />
-        <Checkbox value={false} title='Pinhão' />
+        {products.map((product) => (
+          <Checkbox
+            key={product.id}
+            value={product.checked}
+            title={product.name}
+          />
+        ))}
       </Array>
     </Wrapper>
   )
